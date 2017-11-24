00dad5390e33906190898b7827205b251f1044fc
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2017-11-24 12:32:38 -0300

    cambio de google tag manager
    se administra desde la cuenta empresa de google

--- index.php
+++ index.php
@@ -15,13 +15,18 @@
 <!--*******************************************************************************-->
 <html>
 	<head>
-		<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-53HPW7P');</script>
-<!-- End Google Tag Manager -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110063718-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-110063718-1');
+</script>
+
+
+
 		<title>Tudex Networks</title>
 <link href="images/logo.png" rel="shortcut icon">
 		<meta charset="utf-8" />
